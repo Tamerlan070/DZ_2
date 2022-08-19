b10c5bd87f5bb407c87471fef7a65c224fe09a14
Tamerlan <tamerlan.teshev@icloud.com>
2022-08-20 00:12:43 +0300

    ДЗ выполнено

--- Задача 13/Program.cs
+++ Задача 13/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2){
+  Console.WriteLine("Третья цифра -> " + NumberText[2]);
+}
+else {
+  Console.WriteLine("-> Третья цифра отсутсвует");
+}
--- Задача 15/Program.cs
+++ Задача 15/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введиnt номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("Выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Ошибка, столько дней в недели нет");
+  }
+  else Console.WriteLine("Будничний день");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
